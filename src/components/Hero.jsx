6d8a7e0bb430fc5de4c635e305ae05cae4097fdf
JasonZhu95 <68755176+JasonZhu95@users.noreply.github.com>
2023-04-25 20:20:58 -0700

    Deleted Feedback Component.  Added In the Earth Model and Contact Form

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,8 +27,8 @@ const Hero = () => {
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-            <motion.dev
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
               animate={{
                 y: [0, 24, 0]
               }}
